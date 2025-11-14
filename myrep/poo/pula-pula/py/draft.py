ee6bd79c7d70ec4e503ab295f7be7ae91ee0377b
oHiago <hiagobica@gmail.com>
2025-11-14 15:47:14 -0300

    main do pula-pula quase completa

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -3,38 +3,48 @@
 #Mover a primeira criança que entrou no pula pula para o final da fila de espera.
 
 
-class Pessoa:
-    def __init__(self,nome:str):
+class Criança:
+    def __init__(self,nome:str,idade:int):
         self.__nome = nome
+        self.__idade = idade
     def getNome(self):
         return self.__nome
+    def getIdade(self):
+        return self.__idade
     def __str__(self):
-        return f"{self.__nome}"
+        return f"{self.__nome}:{self.__idade}"
 
 class Pulapula:
     def __init__(self):
-        self.espera: list[Pessoa] = []
-        self.pulapula: list[Pessoa | None] = []
+        self.espera: list[Criança] = []
+        self.pulapula: list[Criança | None] = []
 
-    def fila(self,pessoa: Pessoa):
-        self.espera.append(pessoa)
+    def fila(self,criança: Criança):
+        self.espera.insert(0,criança)
 
     def entrar(self):
-        if self.espera is None:
+        if not self.espera:
             print("fail: ninguem na fila")
             return
         else:
-            self.espera.pop(0)
+            criança = self.espera.pop()
+            self.pulapula.insert(0,criança) 
 
     def sair(self):
-        if self.espera is None:
+        if not self.espera:
             print("fail: ninguem na fila")
             return
         else:
-            self.pulapula.pop(0)
+           criança = self.pulapula.pop()
+           self.espera.insert(0,criança)
+
+    def __str__(self):
+        espera = ", ".join(str(x) for x in self.espera)
+        pulapula = ", ".join(str(x) for x in self.pulapula)
+        return f"[{espera}] => [{pulapula}]"
 
 def main():
-    pula_pula: Pulapula | None = None
+    pula_pula = Pulapula ()
 
     while True:
         line = input()
@@ -44,10 +54,15 @@ def main():
         if args[0] == "end":
             break
         if args[0] == "show":
-            print("pula_pula")
+            print(pula_pula)
         if args[0] == "arrive":
             nome = args[1]
-            pula_pula.fila(Pessoa(nome))
+            idade = int(args[2])
+            pula_pula.fila(Criança(nome,idade))
+        if args[0] == "enter":
+            pula_pula.entrar()
+        if args[0] == "leave":
+            pula_pula.sair()
 
 if __name__ == "__main__":
     main()
